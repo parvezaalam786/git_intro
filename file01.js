5b7cdf3dbfc424f75f8086a42cf4e0ae35bb9c2f
parvezaalam786 <parvezaalam786.p@gmail.com>
2021-01-10 22:20:17 +0530

    sheldon greeter

--- file01.js
+++ file01.js
@@ -1,6 +1,8 @@
 function greet(name)
 {
-    console.log("Hello " + name);
+    console.log("knock knock knock " + name);
+    console.log("knock knock knock " + name);
+    console.log("knock knock knock " + name);
 }
 
 greet("Parvez Ansari")
